b3eaf6e75b0480ba443f36bb1a809a1768b7edf0
lazuardik <andhikarafilazuardi@gmail.com>
2023-11-21 15:28:11 +0700

    commit dictionary and reader

---
+++ katwe.py
@@ -0,0 +1,18 @@
+def check(str):
+    for i in str:
+        if ord(i) < ord('a') or ord(i) > ord('z'):
+            return False
+    return True
+
+f = open("dictionary.txt", "rb").read().decode().split("\n")
+print(len(f))
+
+ans = []
+p = open("dictionary5.txt", "w")
+
+for i in f:
+    if len(i) == 5 and check(i):
+        ans.append(i)
+        p.write(i+"\n")
+p.close()
+
